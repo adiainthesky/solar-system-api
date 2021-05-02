28f08228e8f30496f6d917598597aa2861839716
Adelaide <adiainthesky@gmail.com>
2021-05-02 13:19:22 -0700

    create POST handling of new planets

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,18 @@
-from flask import Blueprint
+from app import db
+from app.models.planet import Planet
+from flask import request, Blueprint, make_response
 
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+
+@planets_bp.route("", methods=["POST"])
+    def handle_planets():
+        request_body = request.get_json()
+        new_planet = Planet(name=request_body["name"],
+                    description=request_body["description"],
+                    distance_from_earth=request_body["distance from Earth"])
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} successfully created", 201)
