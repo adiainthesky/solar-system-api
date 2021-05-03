7450fe2def940ca48e21a244ba90edd6669fae60
Adelaide <adiainthesky@gmail.com>
2021-05-02 21:24:01 -0700

    update the handle_planets function with GET method

--- app/routes.py
+++ app/routes.py
@@ -5,14 +5,41 @@ from flask import request, Blueprint, make_response
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
 
-@planets_bp.route("", methods=["POST"])
+# @planets_bp.route("/<planet_id>", methods=["GET"])
+# def handle_planet(planet_id):
+#     planet = Planet.query.get(planet_id)
+
+#     return {
+#         "id": planet.id,
+#         "title": planet.title,
+#         "description": planet.description
+#         "distance from Earth": planet.distance_from_earth
+#     }
+
+
+@planets_bp.route("", methods=["GET", "POST"])
 def handle_planets():
-    request_body = request.get_json()
-    new_planet = Planet(name=request_body["name"],
-                description=request_body["description"],
-                distance_from_earth=request_body["distance from Earth"])
 
-    db.session.add(new_planet)
-    db.session.commit()
+    if request.method == "GET":
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planets_response.append({
+                "id": planet.id,
+                "title": planet.title,
+                "description": planet.description
+                "distance from Earth": planet.distance_from_earth
+            })
 
-    return make_response(f"Planet {new_planet.name} successfully created", 201)
+        return jsonify(planets_response)
+
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_planet = Planet(name=request_body["name"],
+                    description=request_body["description"],
+                    distance_from_earth=request_body["distance from Earth"])
+
+        db.session.add(new_planet)
+        db.session.commit()
+
+        return make_response(f"Planet {new_planet.name} successfully created", 201)
