724f9b06bd29665a5d71980f862ae46e759b3b6d
Adelaide <adiainthesky@gmail.com>
2021-05-05 18:00:18 -0700

    add test_get_planets_empty

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,4 +1,4 @@
-def test_get_all_planets(client):
+def test_get_all_planets_empty(client):
     response = client.get("/planets")
     response_body = response.get_json()
 
@@ -23,3 +23,15 @@ def test_get_planet_empty(client):
 
     assert response.status_code == 404
     assert response_body == None
+
+def test_get_planets(create_a_planet, client):
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body == [{
+        "id":1,
+        "name":"Earth",
+        "description":"home",
+        "distance from Earth":0
+        }]
